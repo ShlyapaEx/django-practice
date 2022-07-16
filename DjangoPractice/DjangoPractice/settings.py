0891f9ed6537a08d0f19c1e01a8308365e648c6b
ShlyapaEx <musha742@gmail.com>
2022-07-16 20:21:56 +0300

    postgre db init

--- DjangoPractice/DjangoPractice/settings.py
+++ DjangoPractice/DjangoPractice/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'api.apps.ApiConfig'
 ]
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'DjangoPractice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-practice-db', 
+        'USER': 'postgres', 
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
