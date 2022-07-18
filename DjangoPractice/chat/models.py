9ad20b8663caba7d4c3d65f742b5ba13ca67d2ac
ShlyapaEx <musha742@gmail.com>
2022-07-18 21:17:24 +0300

    models update

--- DjangoPractice/chat/models.py
+++ DjangoPractice/chat/models.py
@@ -1,10 +1,53 @@
+from fileinput import filename
+from tkinter import CASCADE
 from django.db import models
+
+
+class Chat(models.Model):
+    """Чат, в котором общаются пользователи"""
+    first_user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    second_user = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = "chat"
+
+
+def chat_directory_path(instance: Chat, filename) -> str:
+    # file will be uploaded to MEDIA_ROOT/chat_<first_user>_<second_user>/<filename>
+    return 'chat_{0}_{1}/{2}'.format(
+        instance.first_user,
+        instance.second_user,
+        filename
+    )
 
 
 class Message(models.Model):
     """Сообщение, отправляемое от одного пользователя к другому"""
+    chat = models.ForeignKey('Chat',
+                             on_delete=models.CASCADE,
+                             verbose_name="Чат, в котором будем осуществляться переписка"
+                             )
     text = models.TextField("текст сообщения", blank=False, max_length=1024)
     sent_at = models.DateTimeField("дата отсылки сообщения", auto_now_add=True)
+    last_updated_at = models.DateTimeField(
+        "дата изменения сообщения", auto_now=True)
+
+    from_user = models.ForeignKey('User', on_delete=models.SET_NULL)
+    to_user = models.ForeignKey('User', on_delete=models.SET_NULL)
 
     class Meta:
         db_table = "message"
+
+
+class Attachment(models.Model):
+    """Вложенные файлы к сообщению"""
+    file_owner = models.ForeignKey('User', on_delete=models.CASCADE)
+    message = models.ForeignKey('Message', on_delete=models.CASCADE)
+
+    file = models.FileField(
+        "Файл, прикреплённый к сообщению", upload_to=chat_directory_path)
+    upload_date = models.DateTimeField(
+        "Дата загрузки файла", auto_now_add=True)
+
+    class Meta:
+        db_table = "attachment"
--- DjangoPractice/personal/models.py
+++ DjangoPractice/personal/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 
+def user_directory_path(instance, filename) -> str:
+    # file will be uploaded to MEDIA_ROOT/user_<login>/<filename>
+    return 'user_{0}/{1}'.format(instance.login, filename)
+
+
 class User(models.Model):
     """Пользователь сайта"""
-    login = models.CharField("логин пользователя", max_length=15)
+    login = models.CharField("логин пользователя", max_length=15, unique=True)
     password = models.CharField("пароль пользователя", max_length=20)
     photo = models.ImageField("аватарка пользователя",
-                              upload_to="user_images/%Y/%m/%d/")
-    creation_date = models.DateTimeField("дата создания пользователя", )
+                              upload_to=user_directory_path)
+    creation_date = models.DateTimeField(
+        "дата создания пользователя", auto_now_add=True)
+    friends = models.ManyToManyField("self")
 
     class Meta:
         db_table = "user"
