05688729528a0daa393a0865cca7bd36c0efdbb8
ShlyapaEx <musha742@gmail.com>
2022-07-18 21:14:50 +0300

    orm models init

--- DjangoPractice/chat/models.py
+++ DjangoPractice/chat/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    """Сообщение, отправляемое от одного пользователя к другому"""
+    text = models.TextField("текст сообщения", blank=False, max_length=1024)
+    sent_at = models.DateTimeField("дата отсылки сообщения", auto_now_add=True)
+
+    class Meta:
+        db_table = "message"
--- DjangoPractice/personal/models.py
+++ DjangoPractice/personal/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """Пользователь сайта"""
+    login = models.CharField("логин пользователя", max_length=15)
+    password = models.CharField("пароль пользователя", max_length=20)
+    photo = models.ImageField("аватарка пользователя",
+                              upload_to="user_images/%Y/%m/%d/")
+    creation_date = models.DateTimeField("дата создания пользователя", )
+
+    class Meta:
+        db_table = "user"
+
+    def __str__(self) -> str:
+        return self.login
