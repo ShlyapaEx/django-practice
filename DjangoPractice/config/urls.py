c675a0cafc7aa6475c2464e661a06086834e186a
ShlyapaEx <musha742@gmail.com>
2022-07-24 17:34:40 +0300

    base urls fix

--- DjangoPractice/config/urls.py
+++ DjangoPractice/config/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.urls')),
-    path('api/', include('apps.chat.urls')),
-    path('api/', include('apps.personal.urls'))
+    path('api/chat/', include('apps.chat.urls')),
+    path('api/personal/', include('apps.personal.urls'))
 ]
