a8164e3b39ae38886d998595abfdf973b3e6153d
ShlyapaEx <musha742@gmail.com>
2022-07-31 17:36:12 +0300

    messages api init

--- DjangoPractice/apps/chat/services.py
+++ DjangoPractice/apps/chat/services.py
@@ -6,3 +6,12 @@ def read_chat_list() -> QuerySet:
     """Возвращает список всех чатов из базы данных в формате QuerySet"""
     queryset = Chat.objects.all()
     return queryset
+
+
+# def read_messages_from_chat_list(chat_id: int | str) -> QuerySet:
+#     queryset = Message.objects.filter(chat=chat_id).all()
+#     return queryset
+
+def read_messages_from_chat_list() -> QuerySet:
+    queryset = Message.objects.all()
+    return queryset
--- DjangoPractice/apps/chat/urls.py
+++ DjangoPractice/apps/chat/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-from .routers import chats_router
+from .routers import chats_router, messages_router
 
 
 urlpatterns = [
-    path('', include(chats_router.urls))
-
+    path('', include(chats_router.urls)),
+    path('', include(messages_router.urls))
 ]
