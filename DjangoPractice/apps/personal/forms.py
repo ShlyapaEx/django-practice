7256142951bfa9dd835ea6ec6fa8d23a4e4b3040
ShlyapaEx <musha742@gmail.com>
2023-03-15 15:22:15 +0300

    photo is now optional in user creation

--- DjangoPractice/apps/personal/forms.py
+++ DjangoPractice/apps/personal/forms.py
@@ -1,8 +1,9 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from apps.personal.models import User
 
 
-""" 
+"""
 Список полей в форме регистрации.
 ВАЖНО!!!: не переставлять местами и не удалять поля username, password1, password2, password
 Они используются в .admin.py в классе CustomUserAdmin
@@ -16,6 +17,9 @@ user_list_fields = ('username', 'password', 'birth_date', 'photo')
 
 
 class CustomUserCreationForm(UserCreationForm):
+    photo = forms.ImageField(required=False,
+                             widget=forms.FileInput(attrs={"id": "image_field",  # you can access your image field with this id for css styling .
+                                                    'style': "height: 100px ; width : 100px ; "}))
 
     class Meta:
         model = User
