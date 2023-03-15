76a8947d7fb07e5fa28c62e30362cde126a29972
ShlyapaEx <musha742@gmail.com>
2023-03-15 15:21:56 +0300

    django-session-auth init

--- DjangoPractice/apps/personal/urls.py
+++ DjangoPractice/apps/personal/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 # from .views import
 
 urlpatterns = [
+    path('session-auth/', include('rest_framework.urls'))
     # path('', main),
     # path('test/', test)
 ]
--- DjangoPractice/config/urls.py
+++ DjangoPractice/config/urls.py
@@ -22,5 +22,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('apps.chat.urls')),
-    # path('api/v1/personal/', include('apps.personal.urls'))
+    path('api/v1/', include('apps.personal.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
