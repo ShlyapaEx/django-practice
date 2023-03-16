ddd18f3f1e81a4761122ef59608a8f78e444cb2c
ShlyapaEx <musha742@gmail.com>
2023-03-16 20:04:50 +0300

    chat list, retrieve methods; custom permissions init

--- DjangoPractice/apps/chat/serializers.py
+++ DjangoPractice/apps/chat/serializers.py
@@ -1,34 +1,29 @@
 from rest_framework import serializers
-from .models import Attachment, Chat, Message
-
-
-class ChatSerializer(serializers.ModelSerializer):
-    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
-
-    def create(self, validated_data):
-        validated_data['owner'] = self.context['request'].user
-        return super().create(validated_data)
-
-    class Meta:
-        model = Chat
-        fields = '__all__'
-
-        # read_only_fields = ['owner']
+from .models import Chat, Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = serializers.PrimaryKeyRelatedField(default=serializers.CurrentUserDefault(),
                                                 read_only=True)
 
-    def create(self, validated_data):
-        validated_data['sender'] = self.context['request'].user
-        return super().create(validated_data)
-
     class Meta:
         model = Message
         fields = "__all__"
 
 
+class ChatSerializer(serializers.ModelSerializer):
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Chat
+        fields = '__all__'
+        # read_only_fields = ('owner',)
+
+
+class DetailChatSerializer(ChatSerializer):
+    messages = MessageSerializer(read_only=True, many=True)
+
+    # read_only_fields = ['owner']
 # class AttachmentSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Attachment
--- DjangoPractice/apps/chat/views.py
+++ DjangoPractice/apps/chat/views.py
@@ -1,16 +1,40 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from .services import read_chat_list, read_messages_from_chat_list
-from .serializers import ChatSerializer, MessageSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .permissions import IsOwnerOrReadOnly
+from .serializers import (ChatSerializer, DetailChatSerializer,
+                          MessageSerializer)
+from .services import (get_chats_for_user, read_chat_list,
+                       read_messages_from_chat_list)
 
 
 class ChatAPIViewSet(viewsets.ModelViewSet):
     queryset = read_chat_list()
     serializer_class = ChatSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def list(self, request):
+        current_user_chats = get_chats_for_user(request.user)
+        serializer = self.get_serializer(current_user_chats, many=True)
+        return Response(data=serializer.data)
+
+    def retrieve(self, request, pk=None):
+        queryset = get_chats_for_user(request.user)
+        chat_object = get_object_or_404(queryset, pk=pk)
+        serializer = DetailChatSerializer(chat_object)
+        return Response(data=serializer.data)
 
 
 class MessageAPIViewSet(viewsets.ModelViewSet):
     queryset = read_messages_from_chat_list()
     serializer_class = MessageSerializer
     permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        current_user = self.request.user
+        serializer.save(sender=current_user)
