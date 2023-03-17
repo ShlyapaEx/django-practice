255e46dd89e10c4e6b3f69a076ace4a8043914d3
ShlyapaEx <musha742@gmail.com>
2023-03-17 12:34:55 +0300

    chat owner permission renamed + some typehinting

--- DjangoPractice/apps/chat/permissions.py
+++ DjangoPractice/apps/chat/permissions.py
@@ -1,17 +1,15 @@
 from rest_framework import permissions
 
+from .models import Chat
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+
+class IsChatOwnerOrReadOnly(permissions.BasePermission):
     """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
+    Разрешение на уровне объекта Chat,
+    позволяющее редактировать его только владельцам объекта.
     """
 
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
+    def has_object_permission(self, request, view, chat: Chat) -> bool:
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        # Instance must have an attribute named `owner`.
-        return obj.owner == request.user
+        return chat.owner == request.user
--- DjangoPractice/apps/chat/views.py
+++ DjangoPractice/apps/chat/views.py
@@ -3,27 +3,27 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsChatOwnerOrReadOnly
 from .serializers import (ChatSerializer, DetailChatSerializer,
                           MessageSerializer)
 from .queries import (get_chats_for_user, read_chat_list,
-                      read_messages_from_chat_list)
+                      read_messages_from_chat_list, )
 
 
 class ChatAPIViewSet(viewsets.ModelViewSet):
     queryset = read_chat_list()
     serializer_class = ChatSerializer
-    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly)
+    permission_classes = (IsAuthenticated, IsChatOwnerOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-    def list(self, request):
+    def list(self, request) -> Response:
         current_user_chats = get_chats_for_user(request.user)
         serializer = self.get_serializer(current_user_chats, many=True)
         return Response(data=serializer.data)
 
-    def retrieve(self, request, pk=None):
+    def retrieve(self, request, pk=None) -> Response:
         queryset = get_chats_for_user(request.user)
         chat_object = get_object_or_404(queryset, pk=pk)
         serializer = DetailChatSerializer(chat_object)
@@ -36,5 +36,6 @@ class MessageAPIViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
-        current_user = self.request.user
-        serializer.save(sender=current_user)
+        serializer.save(sender=self.request.user)
+
+        # return super().list(request, *args, **kwargs)
