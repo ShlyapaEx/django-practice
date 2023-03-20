72f02d3ee0a10b2956d52f9098f1df27b2a47622
ShlyapaEx <musha742@gmail.com>
2023-03-20 12:21:49 +0300

    messages queries more docstrings

--- DjangoPractice/apps/chat/queries.py
+++ DjangoPractice/apps/chat/queries.py
@@ -26,11 +26,20 @@ def get_chats_for_user(user) -> QuerySet:
 
 
 def get_messages_for_chat(chat) -> QuerySet:
+    """
+    Функция get_messages_for_chat принимает объект чата в качестве аргумента 
+    и возвращает QuerySet всех сообщений в этом чате.
+    """
     queryset = Message.objects.select_related('chat').filter(chat=chat).all()
     return queryset
 
 
 def user_is_in_chat_by_chat_id(user, chat_id) -> bool:
+    """
+    Функция user_is_in_chat_by_chat_id принимает объект user и chat_id
+    и возвращает True, если пользователь находится в чате с этим chat_id. 
+    В противном случае фунуция возвращает значение False.
+    """
     chat_found = Chat.objects.prefetch_related('users') \
                              .filter(pk=chat_id, users=user).exists()
     return chat_found
